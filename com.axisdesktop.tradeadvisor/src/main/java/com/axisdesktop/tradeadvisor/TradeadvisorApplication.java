9b9fa7e16bae7ff8e6c11ea9c342346b973ab7d7
fxcoder <barabass@gmail.com>
2018-01-15 20:44:30 +0200

    version 2

---
+++ com.axisdesktop.tradeadvisor/src/main/java/com/axisdesktop/tradeadvisor/TradeadvisorApplication.java
@@ -0,0 +1,12 @@
+package com.axisdesktop.tradeadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeadvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeadvisorApplication.class, args);
+	}
+}

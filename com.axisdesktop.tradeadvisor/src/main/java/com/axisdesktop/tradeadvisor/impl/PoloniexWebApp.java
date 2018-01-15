7373531acc03af5bb69c36661d9607517c5175b2
Anatolii Podlesniuk <ap@stylus.com.ua>
2018-01-15 17:42:39 +0200

    initial commit

---
+++ com.axisdesktop.tradeadvisor/src/main/java/com/axisdesktop/tradeadvisor/impl/PoloniexWebApp.java
@@ -0,0 +1,13 @@
+package com.axisdesktop.tradeadvisor.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoloniexWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoloniexWebApp.class, args);
+	}
+
+}
